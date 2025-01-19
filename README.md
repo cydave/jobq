0ce91f9233581f4548ab0dbc1ef82be368ccecde
dave <dave@cyllective.com>
2025-01-19 17:14:46 +0100

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# jobq
+
+A simple job queue capable of producing and handling new jobs inside of jobs.
+Based on the implementation of [qvl/httpsyet](https://github.com/qvl/httpsyet).
+
+## Example
+
+A simple example on how jobq can be used can be found under [examples/example.go](examples/example.go).
---
+++ jobq.go
@@ -0,0 +1,64 @@
+package jobq
+
+type Job interface {
+	ID() string
+}
+
+type JobQueue struct {
+	wait  chan int
+	jobs  chan Job
+	queue chan Job
+}
+
+func (jq JobQueue) Enqueue(jobs []Job) {
+	if len(jobs) == 0 {
+		return
+	}
+	jq.wait <- len(jobs)
+	go func() {
+		for _, j := range jobs {
+			jq.queue <- j
+		}
+	}()
+}
+
+func (jq JobQueue) Jobs() <-chan Job {
+	return jq.jobs
+}
+
+func (jq JobQueue) MarkJobDone() {
+	jq.wait <- -1
+}
+
+func New() *JobQueue {
+	queueCount := 0
+	wait := make(chan int)
+	jobs := make(chan Job)
+	queue := make(chan Job)
+	processed := map[string]interface{}{}
+
+	go func() {
+		for delta := range wait {
+			queueCount += delta
+			if queueCount == 0 {
+				close(queue)
+			}
+		}
+	}()
+
+	go func() {
+		for j := range queue {
+			if _, ok := processed[j.ID()]; !ok {
+				processed[j.ID()] = struct{}{}
+				jobs <- j
+			} else {
+				wait <- -1
+			}
+		}
+
+		close(jobs)
+		close(wait)
+	}()
+
+	return &JobQueue{wait: wait, jobs: jobs, queue: queue}
+}
